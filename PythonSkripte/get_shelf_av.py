376f25fccc7e01f70d0956c747b63737397920fe
Mschkin <christianbild1@web.de>
2018-03-12 22:20:33 +0100

    add get shelf av

--- PythonSkripte/get_shelf_av.py
+++ PythonSkripte/get_shelf_av.py
@@ -2,4 +2,11 @@ import cv2
 import numpy as np
 import copy
 
-def get_shelf_av():
+
+def get_shelf_av(img, corners, length, hight):
+    pts2 = np.float32([[0, 0], [hight, 0], [0, length], [hight, length]])
+    M = cv2.getPerspectiveTransform(np.float32(
+        corners), pts2)
+    dst = cv2.warpPerspective(img, M, (hight, length))
+    av = np.sum(dst, axis=1)
+    return av
