0a0518bd02597cda7aaccfef2aa1f780bd642b63
Mschkin <christianbild1@web.de>
2018-03-14 19:37:48 +0100

    lots of changes

--- PythonSkripte/FixingLocation.py
+++ PythonSkripte/FixingLocation.py
@@ -1,87 +1,69 @@
-import cv2
 import numpy as np
-import copy
-import itertools
-import matplotlib.pyplot as plt
+
 
 class RegionOfProduct:
-    Productname=""
-    Region=np.zeros(1000)
+    Productname = ""
+    Region = np.zeros(1000)
 
-    def __init__(self,name,ListOfVectors):
-        oldvector=ListOfVectors[0]
-        Region=ListOfVectors[0]-ListOfVectors[0]#copy dimenions
-        Norm =0
+    def __init__(self, name, ListOfVectors):
+        oldvector = ListOfVectors[0]
+        Region = ListOfVectors[0] - ListOfVectors[0]  # copy dimenions
+        Norm = 0 * np.sum(oldvector, axis=0)
         for k in ListOfVectors:
-            Region+= (oldvector-k)**2
-            for l in (oldvector-k)**2:
-                Norm+=l
-            oldvector=k
-        Region/=Norm
-        self.Region=np.sqrt(Region)
-        self.name=name
+            Region += (oldvector - k)**2
+            Norm += np.sum((oldvector - k)**2, axis=0)
+            oldvector = k
+        Norm = np.transpose(np.reshape(np.repeat(Norm, 1000), (3, 1000)))
+        print(np.shape(Norm), np.shape(Region))
+        print(Norm)
+        Region = Region / Norm
+        self.Region = np.sqrt(Region)
+        self.name = name
 
-    def overlap(self,ProductPlacement):
-        return np.vdot(self.Region,ProductPlacement)/3
-        #overlap=0
-        #for k in self.Region*ProductPlacement:
+    def overlap(self, ProductPlacement):
+        return np.vdot(self.Region, ProductPlacement) / 3
+        # overlap=0
+        # for k in self.Region*ProductPlacement:
 #            s in k:
 #                overlap+=s
 #        return overlap
 
+
 class RegionsOfStuff:
-    ListOfProducts=[]
-    NumberOfProducts=ListOfProducts.len()
-    Memory=np.zeros(1000)
-    LastVector=np.zeros(1000)
-    GeometricParamter=0.95      #magic number for series
+    ListOfProducts = []
+    NumberOfProducts = len(ListOfProducts)
+    Memory = np.zeros(1000)
+    LastVector = np.zeros(1000)
+    GeometricParamter = 0.95  # magic number for series
 
-    def __init__(self, NumberOfProducts,ListOfListOfVectors,Geomter=0.95):
-        ListOfProducts=[]
-        for N in range(NumberOfProducts):
-            ListOfProducts.append(RegionOfStuff("{}".format(N),ListOfListOfVector[N]))
+    def __init__(self,  ListOfListOfVector, Geomter=0.95):
+        self.NumberOfProducts = len(ListOfListOfVector)
+        ListOfProducts = []
+        for N in range(self.NumberOfProducts):
+            ListOfProducts.append(RegionOfProduct(
+                "{}".format(N), ListOfListOfVector[N]))
 
-        self.ListOfProducts=ListOfProducts
-        self.NumberOfProducts=N
-        self.Memory=ListOfListOfVector[0][0]-ListOfListOfVector[0][0]
-        self.LastVector=self.Memory
-        self.GeometricParamter=Geomter
+        self.ListOfProducts = ListOfProducts
+        self.Memory = ListOfListOfVector[0][0] - ListOfListOfVector[0][0]
+        self.LastVector = self.Memory
+        self.GeometricParamter = Geomter
 
-    def WhichProduct(SeriesOfVectors)# timing is given by scale
+    def WhichProduct(self, SeriesOfVectors):  # timing is given by scale
                     # could be an issue that we only use geometric series here
-        MaxProbability=0
-        GoodsName=""
+        MaxProbability = 0
+        GoodsName = ""
         for Product in self.ListOfProducts:
-            prob=Product.overlap(SeriesOfVectors)
-            if prob>MaxProbability:  #Problem appears if several products have the same 
-                MaxProbability=prob                                         #overlap
-                GoodsName=Product.Productname
+            prob = Product.overlap(SeriesOfVectors)
+            if prob > MaxProbability:  # Problem appears if several products have the same
+                MaxProbability = prob  # overlap
+                GoodsName = Product.Productname
 
         return GoodsName
 
-    def update(VectorToMemorize):
-        self.Memory*=self.GeometricParamter
-        self.Memory+=(VectorToMemorize-self.LastVector)
-        self.LastVector=VectorToMemorize
+    def update(self, VectorToMemorize):
+        self.Memory *= self.GeometricParamter
+        self.Memory += (VectorToMemorize - self.LastVector)
+        self.LastVector = VectorToMemorize
 
-    def Recall():
-        return WhichProduct(self.Memory)
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    def Recall(self):
+        return self.WhichProduct(self.Memory)
--- PythonSkripte/pipeline.py
+++ PythonSkripte/pipeline.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
 import copy
-import itertools
-import matplotlib.pyplot as plt
-from get_corners import get_corners
-from FixingLocation import RegionOfProduct, RegionsOfStuff
+from Supermarkt.PythonSkripte.get_corners import get_corners
+from Supermarkt.PythonSkripte.FixingLocation import RegionOfProduct, RegionsOfStuff
+from Supermarkt.PythonSkripte.get_shelf_av import get_shelf_av
+
 
 cap = cv2.VideoCapture('20171026_213832.mp4')
 cap.set(cv2.CAP_PROP_POS_FRAMES, 500)
-
+print('corners')
 while(True):
     _, frame = cap.read()
     corners = get_corners(frame, 245)
@@ -16,27 +16,31 @@ while(True):
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+print(corners)
+print('corners')
 items = 0
 allframes = []
 allproducts = []
 while(True):
     _, frame = cap.read()
-    allframes.append(frame)
+    allframes.append(get_shelf_av(frame, corners, 1000, 200))
     # frame=mingziframe
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         items += 1
         allproducts.append(copy.deepcopy(allframes))
         allframes = []
-    if items == 6:
+    if items == 2:
         break
-regions = RegionsOfStuff(6, allframes)
+regions = RegionsOfStuff(allproducts)
 while(True):
     _, frame = cap.read()
     allframes.append(frame)
     # frame=mingziframe
     cv2.imshow('frame', frame)
-
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
+print('corners')
