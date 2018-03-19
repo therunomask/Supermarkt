4bac60e0b0fe2fa02eae0c0fa129725aa80d6bc1
Mschkin <christianbild1@web.de>
2018-03-19 21:16:02 +0100

    added test

--- PythonSkripte/scale_jumps.py
+++ PythonSkripte/scale_jumps.py
@@ -4,7 +4,7 @@ import numpy as np
 class scale:
     weight_memory = []
     is_jumping = False
-    treshhold = 10
+    treshhold = 0.2
 
     def updated(self, new_value):
         self.weight_memory += new_value
@@ -23,10 +23,19 @@ class scale:
         j = 0
         a = [[self.weight_memory[-1]]]
         for num, w in enumerate(self.weight_memory[1::-1]):
-            if abs(j[-1] - w) > 10:
+            if abs(j[-1] - w) > self.treshhold:
                 a.append([w])
                 j += 1
             else:
                 a[j].append(w)
         a[:] = [np.mean(i) for i in a if len(i) > 1]
         return a[0] - a[1]
+
+
+a = np.random.random([100])
+s = scale
+for i in a:
+    s.updated(i)
+    print(i, s.detect_jump())
+    if s.detect_jump():
+        print(s.jump_size())
