13f39e702968083ba156970aa5fef57b43cd070f
oneth <noeth@math.lmu.de>
2018-03-20 20:05:53 +0100

    started including scale into pipeline

--- PythonSkripte/FixingLocation.py
+++ PythonSkripte/FixingLocation.py
@@ -5,8 +5,9 @@ class RegionOfProduct:
     Productname = ""
     Region = np.zeros(1000)
     LocationOnDisplay=[]
+    Number=1
 
-    def __init__(self, name, ListOfVectors):
+    def __init__(self, name, ListOfVectors,M):
         oldvector = ListOfVectors[0]
         Region = ListOfVectors[0] - ListOfVectors[0]  # copy dimenions
         Norm = 0 * np.sum(oldvector, axis=0)
@@ -15,11 +16,12 @@ class RegionOfProduct:
             Norm += np.sum((oldvector - k)**2, axis=0)
             oldvector = k
         Norm = np.transpose(np.reshape(np.repeat(Norm, 1000), (3, 1000)))
-        print(np.shape(Norm), np.shape(Region))
-        print(Norm)
+ #       print(np.shape(Norm), np.shape(Region))
+#        print(Norm)
         Region = Region / Norm
         self.Region = np.sqrt(Region)
         self.name = name
+        self.Number=M
 
     def SetLocationOnDisplay(self,Loc):
         LocationOnDisplay=Loc
@@ -39,12 +41,12 @@ class RegionsOfStuff:
     LastVector = np.zeros(1000)
     GeometricParamter = 0.95  # magic number for series
 
-    def __init__(self,  ListOfListOfVector, Geomter=0.95):
+    def __init__(self,  ListOfListOfVector, ListOfNumbers,Geomter=0.95):
         self.NumberOfProducts = len(ListOfListOfVector)
         ListOfProducts = []
         for N in range(self.NumberOfProducts):
             ListOfProducts.append(RegionOfProduct(
-                "{}".format(N), ListOfListOfVector[N]))
+                "{}".format(N), ListOfListOfVector[N],ListOfNumbers[N]))
 
         self.ListOfProducts = ListOfProducts
         self.Memory = ListOfListOfVector[0][0] - ListOfListOfVector[0][0]
@@ -60,17 +62,19 @@ class RegionsOfStuff:
             beginning=end
         
 
-    def WhichProduct(self, SeriesOfVectors):  # timing is given by scale
+    def WhichProduct(self):  # timing is given by scale
                     # could be an issue that we only use geometric series here
         MaxProbability = 0
         GoodsName = ""
+        probabilities=[]
         for Product in self.ListOfProducts:
-            prob = Product.overlap(SeriesOfVectors)
+            prob = Product.overlap(self.Memory)
+            probabilities.append(prob)
             if prob > MaxProbability:  # Problem appears if several products have the same
                 MaxProbability = prob  # overlap
                 GoodsName = Product.Productname
 
-        return GoodsName
+        return [np.array(probabilities),GoodsName]
 
     def update(self, VectorToMemorize):
         self.Memory *= self.GeometricParamter
--- PythonSkripte/pipeline.py
+++ PythonSkripte/pipeline.py
@@ -5,7 +5,7 @@ from Supermarkt.PythonSkripte.visualisation import drawArea
 from Supermarkt.PythonSkripte.get_corners import get_corners
 from Supermarkt.PythonSkripte.FixingLocation import RegionOfProduct, RegionsOfStuff
 from Supermarkt.PythonSkripte.get_shelf_av import get_shelf_av
-
+from Supermarkt.PythonSkripte.scale_gauss_model.py import GaussModels
 
 cap = cv2.VideoCapture('20171026_213832.mp4')
 cap.set(cv2.CAP_PROP_POS_FRAMES, 500)
@@ -23,21 +23,35 @@ print('corners')
 items = 0
 allframes = []
 allproducts = []
+alljumps=[]
+jumpts_per_product=[]
+scale1 = scale()
 while(True):
+    scale1.update()
     _, frame = cap.read()
     allframes.append(get_shelf_av(frame, corners, 1000, 200))
     # frame=mingziframe
     cv2.imshow('frame', frame)
+    if scale1.detect_jump==True:
+        jumps_per_product.append(np.absolute(scale1.jump_size()))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         items += 1
         allproducts.append(copy.deepcopy(allframes))
         allframes = []
+        alljumps.append(copy.deepcopy(jumps_per_product))
+        jumps_per_product=[]
     if items == 2:
         break
-regions = RegionsOfStuff(allproducts)
+regions = RegionsOfStuff(allproducts,[len(k) for k in alljumps])
+weight_prob=GaussModels(alljumps)
 while(True):
     _, frame = cap.read()
-    allframes.append(frame)
+    scale1.update()
+    regions.update(get_shelf_av(frame, corners, 1000, 200)))
+    if scale1.detect_jump==True:
+        itemtaken=(np.sign(scalle.jump_size())==-1.)
+        probabilities=weight_prob.classifier(np.absolute(scale1.jump_size()))*regions.WhichProduct()[0]
+        N=np.argmax(probabilities)
     for num, stuff in enumerate(regions):
         frame = drawArea(
             frame, corners, stuff.LocationOnDisplay[0], stuff.LocationOnDisplay[1], num % 3, 0.3)
