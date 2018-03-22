36c5e803055ff13ad18fa0e0bff17b2dd4f4dc1b
oneth <noeth@math.lmu.de>
2018-03-22 18:15:16 +0100

    changed command to add audio stream to video

--- PythonSkripte/ReplaceAudio.py
+++ PythonSkripte/ReplaceAudio.py
@@ -4,8 +4,7 @@ import os
 def replace_audio(videoname, finalname, audioname="audio"):
 
     os.system("ffmpeg -i " + videoname + ".avi -i " + audioname +
-              ".wav -c:" + videoname + " copy -map 0:" + videoname + ":0 -map 1:"
-              + audioname + ":0 -shortest " + finalname + ".avi")
+              ".wav -codec copy -shortest" + finalname + ".avi")
 
 
 #replace_audio("v", "a", "newvideo")
