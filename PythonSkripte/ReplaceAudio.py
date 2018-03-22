109476404369cdadb54a0245a116a1b2a49ea2f2
oneth <noeth@math.lmu.de>
2018-03-22 18:32:39 +0100

    wrote function to shorten audio stream

--- PythonSkripte/ReplaceAudio.py
+++ PythonSkripte/ReplaceAudio.py
@@ -7,4 +7,8 @@ def replace_audio(videoname, finalname, audioname="audio"):
               ".wav -codec copy -shortest" + finalname + ".avi")
 
 
+def shorten_audio(inputname, outputname, time1, time2):
+    os.system("ffmpeg -i " + inputname +
+              ".wav -ss {} -to {} -c copy ".format(time1, time2) + outputname + ".wav")
+
 #replace_audio("v", "a", "newvideo")
